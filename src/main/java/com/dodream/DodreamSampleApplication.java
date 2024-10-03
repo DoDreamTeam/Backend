417177445f75169b85cfa2885ac7c92d701dcafe
codesche <codesche@gmail.com>
2024-10-04 08:48:54 +0900

    setting: 프로젝트 세팅 (#3)

---
+++ src/main/java/com/dodream/DodreamSampleApplication.java
@@ -0,0 +1,13 @@
+package com.dodream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DodreamSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DodreamSampleApplication.class, args);
+    }
+
+}
