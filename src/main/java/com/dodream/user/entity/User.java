e2a741f87a0f072cbb02ea229f494dd305653a1f
codesche <codesche@gmail.com>
2024-10-18 15:34:53 +0900

    feat: 소셜 로그인 위해 refreshToken 컬럼 추가 및 로그인 인증을 위해 UserDetails 적용 (#34)

--- src/main/java/com/dodream/user/entity/User.java
+++ src/main/java/com/dodream/user/entity/User.java
@@ -8,34 +8,41 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.time.LocalDateTime;
+import java.util.Collection;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Getter
+@Setter
 @Table(name = "users")
 @Entity
 @EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(updatable = false)
     private Long id;
 
+    // 20241017 User 테이블에 추가
     @Column(nullable = false)
     private String username;
 
     @Column
     private String profileImage;
 
+    // google, naver, kakao
     @Column(nullable = false, length = 10)
     private String provider;
 
@@ -50,9 +57,27 @@ public class User {
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 
+    // 20241017 User 테이블에 추가 (jwt token)
+    @Column(name = "refresh_token", length = 2000)
+    private String refreshToken;
+
     // 테스트 코드
     public User(Long id) {
         this.id = id;
     }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return providerId;
+    }
 }
