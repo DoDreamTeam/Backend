91276e5be0ad2ac16124039457a133e8ab462f43
codesche <codesche@gmail.com>
2024-10-18 15:32:16 +0900

    feat: Redis 캐싱 + 자동 값 등록 위해 어노테이션 적용 (#34)

--- src/main/java/com/dodream/DodreamSampleApplication.java
+++ src/main/java/com/dodream/DodreamSampleApplication.java
@@ -2,8 +2,12 @@ package com.dodream;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing              // 자동 값 등록
+@EnableCaching                  // 레디스 캐싱
 public class DodreamSampleApplication {
 
     public static void main(String[] args) {
