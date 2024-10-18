d0250883bd269b80525754b30d7a5ccb49e44a99
seoyeon-jung <jungluchia1204@gmail.com>
2024-10-18 10:29:52 +0900

    feat: 문제집 전체 조회 api에 카테고리별 조회 조건 추가 (#36)
    [기존에 문제집 전체 조회만 있던 코드에서 카테고리별 조회하는 조건을 추가했습니다.]

--- src/main/java/com/dodream/book/controller/BookController.java
+++ src/main/java/com/dodream/book/controller/BookController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,8 +17,15 @@ public class BookController {
     private final BookService bookService;
 
     @GetMapping("")
-    public ResponseEntity<List<BookResponse>> getAllBooks() {
-        List<BookResponse> bookList = bookService.getBookList();
+    public ResponseEntity<List<BookResponse>> getAllBooks(@RequestParam(value = "category", required = false) String category) {
+        List<BookResponse> bookList = null;
+        if (category != null) {
+            // 카테고리별 조회
+            bookList = bookService.getBookListByCategory(category);
+        } else {
+            // 전체 문제집 조회
+            bookList = bookService.getBookList();
+        }
         return ResponseEntity.ok(bookList);
     }
 
