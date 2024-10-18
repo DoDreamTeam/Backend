4556f84cfa9f5a6af8b2940ac64c2fef0649defb
seoyeon-jung <jungluchia1204@gmail.com>
2024-10-18 10:43:05 +0900

    feat: BookService 카테고리 기능 추가 (#36)
    [카테고리 별로 조회하는 로직을 추가했습니다.]

--- src/main/java/com/dodream/book/service/BookServiceImpl.java
+++ src/main/java/com/dodream/book/service/BookServiceImpl.java
@@ -4,6 +4,7 @@ import com.dodream.book.domain.BookResponse;
 import com.dodream.book.entity.Book;
 import com.dodream.book.repository.BookRepository;
 import com.dodream.book.repository.BookmarkRepository;
+import com.dodream.common.enumtype.Category;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,6 +19,30 @@ public class BookServiceImpl implements BookService {
     public List<BookResponse> getBookList() {
         List<Book> bookList = bookRepository.findAllBySecretFalse();
 
+        return bookList.stream()
+            .map(book -> BookResponse.builder()
+                .id(book.getId())
+                .title(book.getTitle())
+                .username(book.getUser() != null ? book.getUser().getUsername() : null)
+                .bookmarkCount(bookmarkRepository.countByBookAndIsDeletedFalse(book)) // 북마크 수 카운트
+                .category(book.getCategory().name())
+                .createdAt(book.getCreatedAt())
+                .build())
+            .toList();
+    }
+
+    @Override
+    public List<BookResponse> getBookListByCategory(String category) {
+        Category categoryEnum;
+
+        try {
+            categoryEnum = Category.valueOf(category.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            // 잘못된 카테고리인 경우 처리 (예외 던지거나 빈 리스트 반환)
+            return List.of(); // 빈 리스트 반환 예시
+        }
+
+        List<Book> bookList = bookRepository.findAllByCategoryAndSecretFalse(categoryEnum);
         return bookList.stream()
             .map(book -> BookResponse.builder()
                 .id(book.getId())
