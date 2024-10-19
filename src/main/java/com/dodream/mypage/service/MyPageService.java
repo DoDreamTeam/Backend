f90adaaac739f6816ee9912d700b60b1fde066a0
DeepSoupp <mmaba@naver.com>
2024-10-19 13:42:27 +0900

    feat: MyPageService 프로필 수정 추가 (#40)

--- src/main/java/com/dodream/mypage/service/MyPageService.java
+++ src/main/java/com/dodream/mypage/service/MyPageService.java
@@ -7,4 +7,8 @@ public interface MyPageService {
 
     // 사용자 정보 가져오기 (userName , profileImage , userBooks )
     UserInfoResponse getUserInfo(Long userId);
+
+    // 사용자 프로필 수정하기
+    UserInfoResponse updateUserProfile(Long userId, String newUserName, String newProfileImage);
+
 }
