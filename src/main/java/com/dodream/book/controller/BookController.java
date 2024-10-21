b0b7e7c798d37335762a124db2f2bca87fe7030a
seoyeon-jung <jungluchia1204@gmail.com>
2024-10-21 17:38:57 +0900

    feat: 문제집 생성 controller 추가 (#55)

--- src/main/java/com/dodream/book/controller/BookController.java
+++ src/main/java/com/dodream/book/controller/BookController.java
@@ -1,11 +1,16 @@
 package com.dodream.book.controller;
 
+import com.dodream.book.domain.BookRequest;
 import com.dodream.book.domain.BookResponse;
 import com.dodream.book.service.BookService;
+import com.dodream.user.entity.User;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +25,13 @@ public class BookController {
     public ResponseEntity<List<BookResponse>> getAllBooks(@RequestParam(value = "category", required = false) String category) {
         List<BookResponse> bookList = bookService.getBooks(category);
         return ResponseEntity.ok(bookList);
+    }
+
+    @PostMapping("")
+    public ResponseEntity<BookResponse> createBook(@AuthenticationPrincipal User user, @RequestBody BookRequest bookRequest) {
+        bookRequest.setUsername(user.getUsername()); // user 가져오기
+        BookResponse addedBook = bookService.addBook(user, bookRequest);
+        return ResponseEntity.ok(addedBook);
     }
 
     @GetMapping("/search")
