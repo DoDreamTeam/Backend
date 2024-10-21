8309d48e9dcf4c41f52c782ad55682875ccb5b48
DeepSoupp <mmaba@naver.com>
2024-10-21 12:06:44 +0900

    feat: 마이페이지 문제집페이지 컨트롤러 사용자 정보 조회 (#44)

---
+++ src/main/java/com/dodream/mypage/controller/MyPageBookController.java
@@ -0,0 +1,26 @@
+package com.dodream.mypage.controller;
+
+
+import com.dodream.mypage.domain.UserInfoResponse;
+import com.dodream.mypage.service.MyPageBookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/mypage/book")
+public class MyPageBookController {
+
+    private final MyPageBookService myPageBookService;
+
+    // 사용자 정보 가져오기 (userName , profileImage , userBooks)
+    @GetMapping("/{id}")
+    public ResponseEntity<UserInfoResponse> getUserInfoAll(@PathVariable("id") Long id) {
+        UserInfoResponse userInfo = myPageBookService.getUserInfoAll(id);
+        return ResponseEntity.ok(userInfo);
+    }
+}
