4b83bbe50ff403120817e0c4313180be48dda940
DeepSoupp <mmaba@naver.com>
2024-10-22 16:20:53 +0900

    feat: 안쓰는 import 삭제 (#61)

--- src/main/java/com/dodream/mypage/controller/MyPageBookController.java
+++ src/main/java/com/dodream/mypage/controller/MyPageBookController.java
@@ -6,11 +6,9 @@ import com.dodream.mypage.domain.UserInfoResponse;
 import com.dodream.mypage.service.MyPageBookService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
