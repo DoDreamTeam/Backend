7cca6b7ec776c142db76f341c8d78ddb9380d0a4
seoyeon-jung <jungluchia1204@gmail.com>
2024-10-22 15:51:52 +0900

    feat: 북마크 repository 생성 (#62)

--- src/main/java/com/dodream/book/repository/BookmarkRepository.java
+++ src/main/java/com/dodream/book/repository/BookmarkRepository.java
@@ -2,6 +2,8 @@ package com.dodream.book.repository;
 
 import com.dodream.book.entity.Book;
 import com.dodream.book.entity.Bookmark;
+import com.dodream.user.entity.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +12,7 @@ public interface BookmarkRepository extends JpaRepository<Bookmark, Integer> {
 
     // 특정 문제집의 북마크 수 count (is_deleted = false 인 경우에만)
     long countByBookAndIsDeletedFalse(Book book);
+
+    // 북마크하기
+    Optional<Bookmark> findByUserAndBook(User user, Book book);
 }
