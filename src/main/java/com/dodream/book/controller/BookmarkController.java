c62edd30f5308d87f542a181e66bcab98c51431f
seoyeon-jung <jungluchia1204@gmail.com>
2024-10-22 15:49:51 +0900

    Revert "feat: 북마크 컨트롤러 생성 (#62)"
    This reverts commit f06aefb6ca5c3c0417dfa83b8d9162cf82b7ba9a.

--- src/main/java/com/dodream/book/controller/BookmarkController.java
+++ src/main/java/com/dodream/book/controller/BookmarkController.java
@@ -1,26 +1,4 @@
 package com.dodream.book.controller;
 
-import com.dodream.book.domain.BookmarkResponse;
-import com.dodream.book.service.BookmarkService;
-import com.dodream.user.entity.User;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/api/books")
-@RequiredArgsConstructor
 public class BookmarkController {
-    private final BookmarkService bookmarkService;
-
-    @PostMapping("/{id}/bookmark")
-    public ResponseEntity<BookmarkResponse> addBookToBookmark(@AuthenticationPrincipal User user, @PathVariable("id") Long id) {
-        BookmarkResponse bookmarkResponse = bookmarkService.toggleBookmark(user, id);
-        return ResponseEntity.ok(bookmarkResponse);
-    }
-
 }
