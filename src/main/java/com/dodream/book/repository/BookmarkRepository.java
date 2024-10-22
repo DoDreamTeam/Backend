e478240ca7381d457f7783630033ed8c9b686e32
seoyeon-jung <jungluchia1204@gmail.com>
2024-10-22 15:50:09 +0900

    Revert "feat: 북마크 repository 생성 (#62)"
    This reverts commit 1fbfd39d8f998f7228c84082e75008b7440b6136.

--- src/main/java/com/dodream/book/repository/BookmarkRepository.java
+++ src/main/java/com/dodream/book/repository/BookmarkRepository.java
@@ -2,8 +2,6 @@ package com.dodream.book.repository;
 
 import com.dodream.book.entity.Book;
 import com.dodream.book.entity.Bookmark;
-import com.dodream.user.entity.User;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,7 +10,4 @@ public interface BookmarkRepository extends JpaRepository<Bookmark, Integer> {
 
     // 특정 문제집의 북마크 수 count (is_deleted = false 인 경우에만)
     long countByBookAndIsDeletedFalse(Book book);
-
-    // 북마크하기
-    Optional<Bookmark> findByUserAndBook(User user, Book book);
 }
