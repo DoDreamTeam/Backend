19990293418f706d4ff93103ea7f254676e5f96a
seoyeon-jung <jungluchia1204@gmail.com>
2024-10-23 15:41:15 +0900

    fix: git conflict 오류 해결

--- src/main/java/com/dodream/book/repository/BookCommentLikeRepository.java
+++ src/main/java/com/dodream/book/repository/BookCommentLikeRepository.java
@@ -2,6 +2,7 @@ package com.dodream.book.repository;
 
 import com.dodream.book.entity.BookComment;
 import com.dodream.book.entity.BookCommentLike;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
