01976a0714f7adf53f950bf4b3365b4b1e12ea94
codesche <codesche@gmail.com>
2024-10-23 09:33:06 +0900

    feat: 커스텀 예외 처리 (#65)

--- src/main/java/com/dodream/common/exception/BaseException.java
+++ src/main/java/com/dodream/common/exception/BaseException.java
@@ -13,6 +13,9 @@ public class BaseException extends RuntimeException {
     public static final BaseException BOOK_CATEGORY_NOT_FOUND = new BaseException(ErrorCode.BOOK_CATEGORY_NOT_FOUND);
     public static final BaseException BOOK_SEARCH_NOT_FOUND = new BaseException(ErrorCode.BOOK_SEARCH_NOT_FOUND);
     public static final BaseException BOOK_NOT_FOUND = new BaseException(ErrorCode.BOOK_NOT_FOUND);
+    public static final BaseException STUDY_CATEGORY_ERROR = new BaseException(ErrorCode.STUDY_CATEGORY_ERROR);
+    public static final BaseException STUDY_CATEGORY_NOT_FOUND = new BaseException(ErrorCode.STUDY_CATEGORY_NOT_FOUND);
+    public static final BaseException STUDY_SEARCH_NOT_FOUND = new BaseException(ErrorCode.STUDY_SEARCH_NOT_FOUND);
     public static final BaseException INTERNAL_SERVER_ERROR = new BaseException(ErrorCode.INTERNAL_SERVER_ERROR);
     public static final BaseException ACCESS_DENIED = new BaseException(ErrorCode.ACCESS_DENIED);
     public static final BaseException INVALID_TOKEN = new BaseException(ErrorCode.INVALID_TOKEN);
--- src/main/java/com/dodream/common/exception/ErrorCode.java
+++ src/main/java/com/dodream/common/exception/ErrorCode.java
@@ -12,6 +12,7 @@ public enum ErrorCode {
     VALIDATION_FAILED(HttpStatus.BAD_REQUEST, "VALIDATION_FAILED", "입력값 유효성 검사에 실패했습니다."),
     INVALID_TOKEN(HttpStatus.BAD_REQUEST, "INVALID_TOKEN", "유효하지 않은 토큰입니다."),
     BOOK_NOT_FOUND(HttpStatus.BAD_REQUEST, "BOOK_NOT_FOUND", "해당 id를 가진 문제집이 없습니다."),
+    STUDY_NOT_FOUND(HttpStatus.NOT_FOUND, "STUDY_NOT_FOUND", "해당 id를 가진 스터디가 없습니다."),
 
     /* 403 */
     ACCESS_DENIED(HttpStatus.FORBIDDEN, "ACCESS_DENIED", "접근 권한이 없습니다."),
@@ -20,6 +21,9 @@ public enum ErrorCode {
     BOOK_CATEGORY_ERROR(HttpStatus.NOT_FOUND, "BOOK_CATEGORY_ERROR", "해당 카테고리는 존재하지 않습니다"),
     BOOK_CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "BOOK_CATEGORY_NOT_FOUND", "해당 카테고리에 포함된 문제집이 없습니다."),
     BOOK_SEARCH_NOT_FOUND(HttpStatus.NOT_FOUND, "BOOK_SEARCH_NOT_FOUND", "검색어와 일치하는 문제집이 없습니다."),
+    STUDY_CATEGORY_ERROR(HttpStatus.NOT_FOUND, "STUDY_CATEGORY_ERROR", "해당 카테고리는 존재하지 않습니다"),
+    STUDY_CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "STUDY_CATEGORY_NOT_FOUND", "해당 카테고리에 포함된 스터디가 없습니다."),
+    STUDY_SEARCH_NOT_FOUND(HttpStatus.NOT_FOUND, "STUDY_SEARCH_NOT_FOUND", "검색어와 일치하는 스터디가 없습니다."),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USER_NOT_FOUND", "사용자를 찿을 수 없습니다."),
 
     /* 500 */
