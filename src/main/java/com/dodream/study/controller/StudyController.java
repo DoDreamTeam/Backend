28c7254723b0dc83bc497413f551fbc1c2368acd
codesche <codesche@gmail.com>
2024-10-23 09:34:20 +0900

    feat: StudyController 작성 (#65)

--- src/main/java/com/dodream/study/controller/StudyController.java
+++ src/main/java/com/dodream/study/controller/StudyController.java
@@ -1,12 +1,26 @@
 package com.dodream.study.controller;
 
+import com.dodream.study.domain.StudyRequest;
 import com.dodream.study.domain.StudyResponse;
+import com.dodream.study.domain.StudyUpdateRequest;
+import com.dodream.study.domain.StudyUpdateResponse;
 import com.dodream.study.service.StudyService;
-import java.util.List;
+import com.dodream.user.entity.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,13 +30,50 @@ public class StudyController {
 
     private final StudyService studyService;
 
+    // 스터디 전체 조회 (최신순 조회 + 카테고리별 전체 조회)
     @GetMapping("")
-    public ResponseEntity<List<StudyResponse>> getStudyList() {
-        // 스프링 시큐리티를 통해 사용자 정보 가져오기
-//        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-//        List<StudyResponse> result = studyService.getStudyList(authentication);
-        List<StudyResponse> result = studyService.getStudyList();
-        return ResponseEntity.ok(result);
+    public ResponseEntity<Page<StudyResponse>> getStudyList(
+        @PageableDefault(page = 0, size = 12, sort = "createdAt",
+            direction = Sort.Direction.DESC) Pageable pageable,
+        @AuthenticationPrincipal User user,
+        @RequestParam(value = "category", required = false) String category) {
+        Page<StudyResponse> response = studyService.getStudyList(user, pageable, category);
+        return ResponseEntity.ok(response);
+    }
+
+    // 스터디 검색 조회
+    @GetMapping("/search")
+    public ResponseEntity<Page<StudyResponse>> searchStudies(
+        @PageableDefault(page = 0, size = 12, sort = "updateAt",
+            direction = Sort.Direction.DESC) Pageable pageable,
+        @RequestParam(value = "keyword", required = false) String keyword) {
+        Page<StudyResponse> response = studyService.searchStudiesByKeyword(pageable, keyword);
+        return ResponseEntity.ok(response);
+    }
+
+    // 스터디 생성 - 공지사항 추가 필요
+    @PostMapping("")
+    public ResponseEntity<StudyResponse> createStudy(@AuthenticationPrincipal User user,
+        @RequestBody StudyRequest studyRequest) {
+        studyRequest.setUsername(user.getUsername());
+        StudyResponse studyResponse = studyService.addStudy(user, studyRequest);
+        return ResponseEntity.ok(studyResponse);
+    }
+
+    // 스터디 삭제
+    @DeleteMapping("/{id}")
+    public ResponseEntity<StudyResponse> deleteStudy(@AuthenticationPrincipal User user,
+        @PathVariable("id") Long id) {
+        studyService.deleteStudy(user, id);
+        return ResponseEntity.noContent().build();
+    }
+
+    // 스터디 수정 (추후 공지사항 컬럼도 같이 추가)
+    @PatchMapping("/{id}")
+    public ResponseEntity<StudyUpdateResponse> updateStudy(@AuthenticationPrincipal User user,
+        @PathVariable("id") Long id, @RequestBody StudyUpdateRequest studyUpdateRequest) {
+        StudyUpdateResponse updateResponse = studyService.updateStudy(user, id ,studyUpdateRequest);
+        return ResponseEntity.ok(updateResponse);
     }
 
 }
