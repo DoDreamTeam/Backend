66a9db05620342301cb681c4a8836f68f736eb24
codesche <codesche@gmail.com>
2024-10-23 09:35:50 +0900

    feat: StudyRepository 작성 (#65)

--- src/main/java/com/dodream/study/repository/StudyRepository.java
+++ src/main/java/com/dodream/study/repository/StudyRepository.java
@@ -1,9 +1,13 @@
 package com.dodream.study.repository;
 
 import com.dodream.common.enumtype.Category;
+import com.dodream.study.domain.StudyResponse;
 import com.dodream.study.entity.Study;
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -12,4 +16,35 @@ public interface StudyRepository extends JpaRepository<Study, Long> {
 
     List<Study> findByTitleContainsOrDescriptionContainsOrderByTitleAsc(
         String titleKeyword, String descriptionKeyword);
+
+    /* 스터디 카테고리별로 검색 */
+    @Query("SELECT new com.dodream.study.domain.StudyResponse(s.id, s.title, s.user.username, "
+        + "s.description, s.category, null, s.updatedAt, s.createdAt, "
+        + "(SELECT COUNT(sm) FROM StudyMember sm WHERE sm.study.id = s.id)) "
+        + "FROM Study s "
+        + "WHERE (:category IS NULL OR s.category = :category) ")
+    Page<StudyResponse> findByStudyCategory(Pageable pageable, Category category);
+
+    /* 스터디 키워드(제목 + 내용 or 사용자) 로 검색 */
+    @Query("SELECT new com.dodream.study.domain.StudyResponse(s.id, s.title, s.user.username, "
+        + "s.description, s.category, null, s.updatedAt, s.createdAt,"
+        + "(SELECT COUNT(sm) FROM StudyMember sm WHERE sm.study.id = s.id)) "
+        + "FROM Study s "
+        + "WHERE LOWER(s.title) LIKE LOWER(CONCAT('%', :keyword, '%')) "
+        + "OR LOWER(s.description) LIKE LOWER(CONCAT('%', :keyword, '%')) "
+        + "OR LOWER(s.user.username) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+    Page<StudyResponse> findStudiesByTitleDescriptionOrUsername(Pageable pageable, String keyword);
+
+    /* 스터디 전체 조회 */
+    @Query("SELECT new com.dodream.study.domain.StudyResponse(s.id, s.title, s.user.username, "
+        + "s.description, s.category, null, s.updatedAt, s.createdAt, "
+        + "(SELECT COUNT(sm) FROM StudyMember sm WHERE sm.study.id = s.id)) FROM Study s")
+    Page<StudyResponse> findAllStudy(Pageable pageable);
+
+    /* 인기 스터디 조회 */
+    @Query("SELECT new com.dodream.study.domain.StudyResponse(s.id, s.title, s.user.username, "
+        + "s.description, s.category, null, s.updatedAt, s.createdAt as update, "
+        + "(SELECT COUNT(sm) FROM StudyMember sm WHERE sm.study.id = s.id) as count ) FROM Study s "
+        + "ORDER BY count DESC, update DESC ")
+    Page<StudyResponse> findAllStudyWithMemberCount(Pageable pageable);
 }
