f7dfb7a93c90dacefb144426944e473c08b9ef38
DeepSoupp <mmaba@naver.com>
2024-10-23 12:21:41 +0900

    feat: 문제집 댓글 , 좋아요 조회 기능 구현 (#61)

--- src/main/java/com/dodream/mypage/service/MyPageBookService.java
+++ src/main/java/com/dodream/mypage/service/MyPageBookService.java
@@ -1,14 +1,24 @@
 package com.dodream.mypage.service;
 
+import com.dodream.book.entity.BookComment;
+import com.dodream.mypage.domain.BookCommentLikeResponse;
+import com.dodream.mypage.domain.BookCommentResponse;
 import com.dodream.mypage.domain.BookUpdateResponse;
 import com.dodream.mypage.domain.UserInfoResponse;
+import java.util.List;
 
 
 public interface MyPageBookService {
 
     // 사용자 정보 가져오기 (userName , profileImage , userBooks )
-    UserInfoResponse getUserInfoAll(Long userId);
+    UserInfoResponse getUserInfoAll();
 
     // 문제집 공개 비공개 설정
     BookUpdateResponse updateSecret(Long bookId);
+
+    // 마이페이지 문제집 댓글 목록 조회
+    List<BookCommentResponse> getUserComment();
+
+    // 문제집 댓글 좋아요 목록 조회
+    List<BookCommentLikeResponse> getUserCommentLike();
 }
