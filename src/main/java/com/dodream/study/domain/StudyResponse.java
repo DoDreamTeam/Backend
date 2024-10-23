6e1bf8bc9b0f57e19e93d9d72458b41c53b4fbea
codesche <codesche@gmail.com>
2024-10-23 09:36:48 +0900

    feat: StudyResponse 직렬화/역직렬화 + 생성자 어노테이션 추가 (#65)

--- src/main/java/com/dodream/study/domain/StudyResponse.java
+++ src/main/java/com/dodream/study/domain/StudyResponse.java
@@ -1,19 +1,39 @@
 package com.dodream.study.domain;
 
-import com.dodream.study.entity.Study;
-import com.dodream.user.entity.User;
-import java.util.Locale.Category;
+import com.dodream.common.enumtype.Category;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class StudyResponse {
     private Long id;
     private String title;           // 2024년 정처기 실기 2회 대비
     private String username;        // test
     private String description;     // 스터디에 대한 아주 간단한 설명
-    private String category;        // 자격증
+    private Category category;        // 자격증
     private String status;          // 신청, 승인대기중, 참여중
-    private Long userCount;         // 멤버 수 -> StudyMember Id row 개수 (Member, Leader 포함)
+
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime updatedAt;
+
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt;
+
+    private Long userCount;             // 멤버 수 -> StudyMember Id row 개수 (Member, Leader 포함)
+
 }
