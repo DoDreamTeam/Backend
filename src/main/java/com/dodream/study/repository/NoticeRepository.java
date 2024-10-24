28e9a375d59f43cca39ca8c9cd773bb4b864d3a9
codesche <codesche@gmail.com>
2024-10-24 16:32:22 +0900

    feat: NoticeRepository 생성 (#85)

---
+++ src/main/java/com/dodream/study/repository/NoticeRepository.java
@@ -0,0 +1,17 @@
+package com.dodream.study.repository;
+
+import com.dodream.study.entity.Notice;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NoticeRepository extends JpaRepository<Notice, Long> {
+    @Query("SELECT n FROM Notice n WHERE n.study.id = :studyId AND n.id = :noticeId AND n.isDeleted = false")
+    Optional<Notice> findByStudyIdAndNoticeId(Long studyId, Long noticeId);
+
+    @Query("SELECT n FROM Notice n WHERE n.study.id = :studyId AND n.isDeleted = true AND n.content = ''")
+    Optional<Notice> findDeletedEmptyContentNoticeByStudyId(Long studyId);
+
+}
