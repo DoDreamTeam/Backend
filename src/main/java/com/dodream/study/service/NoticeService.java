1913b6ce4f48cbd2274d3a417d4241fb5d6a2af8
codesche <codesche@gmail.com>
2024-10-24 16:32:44 +0900

    feat: NoticeService 생성 (#85)

---
+++ src/main/java/com/dodream/study/service/NoticeService.java
@@ -0,0 +1,14 @@
+package com.dodream.study.service;
+
+import com.dodream.study.domain.NoticeRequest;
+import com.dodream.study.domain.NoticeResponse;
+import com.dodream.study.domain.UpdateNoticeRequest;
+import com.dodream.study.domain.UpdateNoticeResponse;
+import com.dodream.user.entity.User;
+
+public interface NoticeService {
+    NoticeResponse createNotice(Long studyId, NoticeRequest noticeRequest, User user);
+    UpdateNoticeResponse updateNotice(Long noticeId, UpdateNoticeRequest updateNoticeRequest, User user);
+    UpdateNoticeResponse deleteNotice(Long noticeId, User user);
+    NoticeResponse getNoticeByStudyIdAndNoticeId(Long studyId, Long noticeId);
+}
