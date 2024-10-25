c59e119c6cf0bf3e5cdeae862fda28066c63fdf2
seoyeon-jung <jungluchia1204@gmail.com>
2024-10-25 11:31:54 +0900

    feat: 문제 삭제 controller 추가 (#90)

--- src/main/java/com/dodream/book/controller/QuestionController.java
+++ src/main/java/com/dodream/book/controller/QuestionController.java
@@ -7,6 +7,7 @@ import com.dodream.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,6 +26,14 @@ public class QuestionController {
         @AuthenticationPrincipal User user, @RequestBody QuestionRequest questionRequest) {
         QuestionResponse addedQuestion = questionService.addQuestion(id, user, questionRequest);
         return ResponseEntity.ok(addedQuestion);
+    }
+
+    // 문제 삭제
+    @DeleteMapping("/{id}/questions/{questionId}")
+    public ResponseEntity<QuestionResponse> deleteQuestion(@PathVariable("id") Long id,
+        @PathVariable("questionId") Long questionId, @AuthenticationPrincipal User user) {
+        questionService.deleteQuestion(id, questionId, user);
+        return ResponseEntity.noContent().build(); // 204 No Content
     }
 
 }
