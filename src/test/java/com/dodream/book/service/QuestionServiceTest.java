b9bdb96ebe06e9b74815e0c2e0e73f9ae7d5a78d
seoyeon-jung <jungluchia1204@gmail.com>
2024-10-25 14:19:17 +0900

    test: service test code 추가 (#90)

--- src/test/java/com/dodream/book/service/QuestionServiceTest.java
+++ src/test/java/com/dodream/book/service/QuestionServiceTest.java
@@ -4,11 +4,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import com.dodream.book.domain.QuestionRequest;
-import com.dodream.book.domain.QuestionResponse;
+import com.dodream.book.domain.QuestionAddRequest;
+import com.dodream.book.domain.QuestionAddResponse;
+import com.dodream.book.domain.QuestionListResponse;
 import com.dodream.book.entity.Book;
 import com.dodream.book.entity.Question;
 import com.dodream.book.repository.BookRepository;
@@ -18,6 +20,7 @@ import com.dodream.common.exception.ErrorCode;
 import com.dodream.user.entity.User;
 import com.dodream.user.repository.UserRepository;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -26,6 +29,10 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
 public class QuestionServiceTest {
 
@@ -43,7 +50,7 @@ public class QuestionServiceTest {
 
     private User user;
     private Book book;
-    private QuestionRequest questionRequest;
+    private QuestionAddRequest questionRequest;
 
     @BeforeEach
     void setUp() {
@@ -55,7 +62,7 @@ public class QuestionServiceTest {
         book.setId(1L);
         book.setUser(user); // 문제집 소유자 설정
 
-        questionRequest = new QuestionRequest();
+        questionRequest = new QuestionAddRequest();
         questionRequest.setQuestion("Sample question");
         questionRequest.setModelAnswer("Sample answer");
     }
@@ -75,7 +82,7 @@ public class QuestionServiceTest {
                 .build();
         });
 
-        QuestionResponse response = questionService.addQuestion(1L, user, questionRequest);
+        QuestionAddResponse response = questionService.addQuestion(1L, user, questionRequest);
 
         assertNotNull(response);
         assertEquals("Sample question", response.getQuestion());
@@ -163,4 +170,45 @@ public class QuestionServiceTest {
             questionService.deleteQuestion(1L, 1L, user));
         assertEquals(ErrorCode.QUESTION_NOT_FOUND, exception.getErrorCode());
     }
+
+    @DisplayName("문제 전체 조회 성공")
+    @Test
+    void testGetQuestions_Success() {
+        // Given
+        Question question1 = Question.builder()
+            .id(1L)
+            .question("Question 1")
+            .modelAnswer("Answer 1")
+            .book(book)
+            .createdAt(LocalDateTime.now().minusDays(1))
+            .build();
+
+        Question question2 = Question.builder()
+            .id(2L)
+            .question("Question 2")
+            .modelAnswer("Answer 2")
+            .book(book)
+            .createdAt(LocalDateTime.now())
+            .build();
+
+        List<Question> questionList = List.of(question1, question2);
+        Page<Question> questionPage = new PageImpl<>(questionList, PageRequest.of(0, 2), questionList.size());
+
+        when(questionRepository.findByBookIdOrderByCreatedAtDesc(any(Pageable.class), eq(book.getId())))
+            .thenReturn(questionPage);
+
+        // When
+        Page<QuestionListResponse> result = questionService.getQuestions(PageRequest.of(0, 2), book.getId());
+
+        // Then
+        assertNotNull(result);
+        assertEquals(2, result.getTotalElements());
+        assertEquals(1, result.getTotalPages());
+
+        List<QuestionListResponse> questionResponses = result.getContent();
+        assertEquals(2, questionResponses.size());
+        assertEquals("Question 1", questionResponses.get(0).getQuestion());
+        assertEquals("Question 2", questionResponses.get(1).getQuestion());
+    }
+
 }
