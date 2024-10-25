3c6420737772fd294beb112bc4a55d9a790cc345
DeepSoupp <mmaba@naver.com>
2024-10-25 09:47:48 +0900

    feat: 사용자의 문제 댓글 좋아요 기능 Response 추가 (#76)

---
+++ src/main/java/com/dodream/mypage/domain/QueCommentLikeResponse.java
@@ -0,0 +1,20 @@
+package com.dodream.mypage.domain;
+
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class QueCommentLikeResponse {
+
+    private Long commentId; // 댓글 ID
+    private String comment; // 댓글 내용
+    private Long userId; // 사용자 ID
+    private String userName; // 사용자 이름
+    private LocalDateTime createdAt; // 생성날짜
+}
