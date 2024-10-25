c31a10298449a392ac1c478bccd2812c024e9aba
DeepSoupp <mmaba@naver.com>
2024-10-25 16:09:40 +0900

    feat: 스터디 문제 조회 Response 추가 (#89)

---
+++ src/main/java/com/dodream/study/domain/StudyUserAnswerResponse.java
@@ -0,0 +1,15 @@
+package com.dodream.study.domain;
+
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class StudyUserAnswerResponse {
+    private Long id;
+    private String question;
+    private String modelAnswer;
+    private String answer;
+    private LocalDateTime createdAt;
+}
