00254236cd8022e1afa520bc82f19d2ac6c45181
seoyeon-jung <jungluchia1204@gmail.com>
2024-10-25 14:17:27 +0900

    feat: 문제 전체 조회시 필요한 response 추가 (#90)
    [문제 전체 조회 시 문제, 생성 날짜가 필요하므로 따로 response를 추가하였습니다.]

---
+++ src/main/java/com/dodream/book/domain/QuestionListResponse.java
@@ -0,0 +1,16 @@
+package com.dodream.book.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class QuestionListResponse {
+    private Long id;                    // 문제 ID
+    private String question;            // 문제
+
+    @JsonFormat(shape= JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt;    // 생성 날짜
+}
