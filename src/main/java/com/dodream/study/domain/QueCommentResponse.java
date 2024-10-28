8ce3c8fcb4024b086ffabb321248f84941939aee
DeepSoupp <mmaba@naver.com>
2024-10-28 15:15:30 +0900

    feat: 스터디 문제 댓글 CRUD domain 추가 (#95)

---
+++ src/main/java/com/dodream/study/domain/QueCommentResponse.java
@@ -0,0 +1,32 @@
+package com.dodream.study.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class QueCommentResponse {
+
+    private Long id;
+    private String content;
+    private String username;
+    private Long userAnswerId;
+    private Long likeCount;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    private LocalDateTime createdAt;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
+    private LocalDateTime updatedAt;
+}
---
+++ src/main/java/com/dodream/study/domain/QueCommentUpdateRequest.java
@@ -0,0 +1,8 @@
+package com.dodream.study.domain;
+
+import lombok.Data;
+
+@Data
+public class QueCommentUpdateRequest {
+    private String content;
+}
