2adf33080492ee9d3210957f1081f9b312e23c73
DeepSoupp <mmaba@naver.com>
2024-10-28 15:44:44 +0900

    feat: 스터디 문제 댓글 좋아요 domain 추가 (#95)

---
+++ src/main/java/com/dodream/study/domain/QueCommentLikeResponse.java
@@ -0,0 +1,18 @@
+package com.dodream.study.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class QueCommentLikeResponse {
+    private Long id;
+    private boolean isDeleted;
+    private Long userId;
+    private Long queComment;
+
+}
