f36577f0500b122b3ac7c54a49111d8870193ff0
seoyeon-jung <jungluchia1204@gmail.com>
2024-10-28 11:22:12 +0900

    feat: controller 구현 (#98)

---
+++ src/main/java/com/dodream/book/controller/UserAnswerController.java
@@ -0,0 +1,61 @@
+package com.dodream.book.controller;
+
+import com.dodream.book.domain.AnswerDetailsResponse;
+import com.dodream.book.domain.EvaluationRequest;
+import com.dodream.book.domain.UserAnswerRequest;
+import com.dodream.book.domain.UserAnswerResponse;
+import com.dodream.book.service.UserAnswerService;
+import com.dodream.user.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/books")
+@RequiredArgsConstructor
+public class UserAnswerController {
+    private final UserAnswerService userAnswerService;
+
+    // 문제 풀기
+    // 만약 문제를 다시 푸는 경우 해당 answer와 evaluation이 다시 업데이트 되도록 구현 완료
+    @PostMapping("/{id}/questions/{questionId}/answer")
+    public ResponseEntity<UserAnswerResponse> solveQuestion(
+        @PathVariable("id") Long id,
+        @PathVariable("questionId") Long questionId,
+        @AuthenticationPrincipal User user,
+        @RequestBody UserAnswerRequest userAnswerRequest) {
+        UserAnswerResponse response = userAnswerService.solveQuestion(id, questionId, user, userAnswerRequest);
+        return ResponseEntity.ok(response);
+    }
+
+    // 문제 평가하기
+    @PatchMapping("/{id}/questions/{questionId}/answer/{answerId}")
+    public ResponseEntity<UserAnswerResponse> evaluateAnswer(
+        @PathVariable("id") Long id,
+        @PathVariable("questionId") Long questionId,
+        @PathVariable("answerId") Long answerId,
+        @RequestBody EvaluationRequest evaluationRequest) {
+
+        UserAnswerResponse response = userAnswerService.evaluateAnswer(answerId, evaluationRequest.getEvaluation());
+        return ResponseEntity.ok(response);
+    }
+
+    // 문제 풀고 문제 조회 (문제+모범답안+내답안)
+    @GetMapping("/{id}/questions/{questionId}/answer/{answerId}")
+    public ResponseEntity<AnswerDetailsResponse> getAnswerDetails(
+        @PathVariable("id") Long bookId,
+        @PathVariable("questionId") Long questionId,
+        @PathVariable("answerId") Long answerId,
+        @AuthenticationPrincipal User user) {
+
+        AnswerDetailsResponse response = userAnswerService.getAnswerDetails(bookId, questionId, answerId, user);
+        return ResponseEntity.ok(response);
+    }
+}
