4d7bcb45857f04e120ceb0e764d1fff320b3168f
codesche <codesche@gmail.com>
2024-10-29 04:34:14 +0900

    feature: studyMember DTO (#106)

--- src/main/java/com/dodream/study/domain/StudyMemberResponse.java
+++ src/main/java/com/dodream/study/domain/StudyMemberResponse.java
@@ -1,6 +1,10 @@
 package com.dodream.study.domain;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Data;
@@ -9,9 +13,12 @@ import lombok.Data;
 @Builder
 public class StudyMemberResponse {
 
+    private Long id;
     private String username;
 
     @JsonFormat(shape= JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss")
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime joinDate;
 
 }
