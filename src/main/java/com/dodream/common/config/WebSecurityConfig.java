4fafb32aa25a9f415fc85eacfed413513988df5a
seoyeon-jung <jungluchia1204@gmail.com>
2024-10-28 17:09:49 +0900

    feat: 비로그인시에도 검색할 수 있도록 path 추가 (#105)

--- src/main/java/com/dodream/common/config/WebSecurityConfig.java
+++ src/main/java/com/dodream/common/config/WebSecurityConfig.java
@@ -43,6 +43,9 @@ public class WebSecurityConfig {
                     // 로그인/로그아웃
                     new AntPathRequestMatcher("/api/oauth/**"),
 
+                    // 메인 페이지에서 문제집+스터디 검색
+                    new AntPathRequestMatcher("/api/search", "GET"),
+
                     // 모든 문제집 조회 (최신순)
                     new AntPathRequestMatcher("/api/books", "GET"),
 
