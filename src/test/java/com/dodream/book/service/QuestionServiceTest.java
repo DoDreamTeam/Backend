d0a82da5145f88b763a6fe58d9cf536cfa3062f3
seoyeon-jung <jungluchia1204@gmail.com>
2024-10-29 12:25:21 +0900

    test: question service test code 추가 (#113)

--- src/test/java/com/dodream/book/service/QuestionServiceTest.java
+++ src/test/java/com/dodream/book/service/QuestionServiceTest.java
@@ -4,6 +4,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyList;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.verify;
@@ -14,12 +15,14 @@ import com.dodream.book.domain.QuestionResponse;
 import com.dodream.book.domain.QuestionListResponse;
 import com.dodream.book.entity.Book;
 import com.dodream.book.entity.Question;
+import com.dodream.book.entity.UserAnswer;
 import com.dodream.book.repository.BookRepository;
 import com.dodream.book.repository.QuestionRepository;
 import com.dodream.book.repository.UserAnswerRepository;
 import com.dodream.book.repository.UserBookRepository;
 import com.dodream.common.exception.BaseException;
 import com.dodream.common.exception.ErrorCode;
+import com.dodream.study.repository.StudyUserAnswerRepository;
 import com.dodream.user.entity.User;
 import com.dodream.user.repository.UserRepository;
 import java.time.LocalDateTime;
@@ -49,12 +52,17 @@ public class QuestionServiceTest {
     private BookRepository bookRepository;
 
     @Mock
-    private UserBookRepository userBookRepository; // Added
+    private UserBookRepository userBookRepository;
+
     @Mock
-    private UserAnswerRepository userAnswerRepository; // Added
+    private UserAnswerRepository userAnswerRepository;
+
+    @Mock
+    private StudyUserAnswerRepository studyUserAnswerRepository;
 
     private User user;
     private Book book;
+    private Question question;
     private QuestionRequest questionRequest;
 
     @BeforeEach
@@ -66,15 +74,25 @@ public class QuestionServiceTest {
         book.setId(1L);
         book.setUser(user); // 문제집 소유자 설정
 
-        questionRequest = new QuestionRequest();
-        questionRequest.setQuestion("Sample question");
-        questionRequest.setModelAnswer("Sample answer");
+        question = Question.builder()
+            .id(1L)
+            .question("Sample question")
+            .createdAt(LocalDateTime.now())
+            .book(book)
+            .build();
     }
 
     @DisplayName("문제 생성 성공")
     @Test
     void testAddQuestion_Success() {
+        // Given
         when(bookRepository.findById(1L)).thenReturn(Optional.of(book));
+
+        // Initialize questionRequest with required values
+        QuestionRequest questionRequest = new QuestionRequest();
+        questionRequest.setQuestion("Sample question");
+        questionRequest.setModelAnswer("Sample answer");
+
         when(questionRepository.save(any(Question.class))).thenAnswer(invocation -> {
             Question question = invocation.getArgument(0);
             return Question.builder()
@@ -86,8 +104,10 @@ public class QuestionServiceTest {
                 .build();
         });
 
+        // When
         QuestionResponse response = questionService.addQuestion(1L, user, questionRequest);
 
+        // Then
         assertNotNull(response);
         assertEquals("Sample question", response.getQuestion());
         assertEquals("Sample answer", response.getModelAnswer());
@@ -179,40 +199,98 @@ public class QuestionServiceTest {
     @Test
     void testGetQuestions_Success() {
         // Given
-        Question question1 = Question.builder()
-            .id(1L)
-            .question("Question 1")
-            .modelAnswer("Answer 1")
-            .book(book)
-            .createdAt(LocalDateTime.now().minusDays(1))
-            .build();
+        Pageable pageable = PageRequest.of(0, 10);
+        List<Question> questions = List.of(question);
+        Page<Question> questionPage = new PageImpl<>(questions, pageable, questions.size());
 
-        Question question2 = Question.builder()
-            .id(2L)
-            .question("Question 2")
-            .modelAnswer("Answer 2")
-            .book(book)
-            .createdAt(LocalDateTime.now())
-            .build();
-
-        List<Question> questionList = List.of(question1, question2);
-        Page<Question> questionPage = new PageImpl<>(questionList, PageRequest.of(0, 2), questionList.size());
-
-        when(questionRepository.findByBookIdOrderByCreatedAtDesc(any(Pageable.class), eq(book.getId())))
-            .thenReturn(questionPage);
+        when(questionRepository.findByBookIdOrderByCreatedAtDesc(pageable, 1L)).thenReturn(questionPage);
 
         // When
-        Page<QuestionListResponse> result = questionService.getQuestions(PageRequest.of(0, 2), book.getId(), user, null);
+        Page<QuestionListResponse> result = questionService.getQuestions(pageable, 1L, user, null);
 
         // Then
         assertNotNull(result);
-        assertEquals(2, result.getTotalElements());
+        assertEquals(1, result.getTotalElements());
         assertEquals(1, result.getTotalPages());
+        assertEquals("Sample question", result.getContent().get(0).getQuestion());
+    }
 
-        List<QuestionListResponse> questionResponses = result.getContent();
-        assertEquals(2, questionResponses.size());
-        assertEquals("Question 1", questionResponses.get(0).getQuestion());
-        assertEquals("Question 2", questionResponses.get(1).getQuestion());
+    @DisplayName("문제 전체 조회 시 사용자가 푼 문제 제외")
+    @Test
+    void testGetQuestions_ExcludeAnsweredQuestions() {
+        // Given
+        Pageable pageable = PageRequest.of(0, 10);
+        List<Question> questions = List.of(question);
+        Page<Question> questionPage = new PageImpl<>(questions, pageable, questions.size());
+
+        when(questionRepository.findByBookIdOrderByCreatedAtDesc(pageable, 1L)).thenReturn(questionPage);
+
+        when(userAnswerRepository.findByUserIdAndQuestionIdIn(eq(user.getId()), anyList()))
+            .thenReturn(List.of(
+                UserAnswer.builder()
+                    .question(question)
+                    .user(user)
+                    .build()
+            ));
+
+        // When
+        Page<QuestionListResponse> result = questionService.getQuestions(pageable, 1L, user, true);
+
+        // Then
+        assertNotNull(result);
+        assertEquals(1, result.getTotalElements());
+    }
+
+    @DisplayName("문제 전체 조회 시 문제집이 없는 경우 예외 발생")
+    @Test
+    void testGetQuestions_BookNotFound() {
+        Pageable pageable = PageRequest.of(0, 10);
+
+        when(questionRepository.findByBookIdOrderByCreatedAtDesc(pageable, 1L)).thenThrow(new BaseException(ErrorCode.BOOK_NOT_FOUND));
+
+        BaseException exception = assertThrows(BaseException.class, () ->
+            questionService.getQuestions(pageable, 1L, user, null));
+
+        assertEquals(ErrorCode.BOOK_NOT_FOUND, exception.getErrorCode());
+    }
+
+    @DisplayName("문제 개별 조회 성공")
+    @Test
+    void testGetOneQuestion_Success() {
+        // Given
+        when(bookRepository.findById(1L)).thenReturn(Optional.of(book));
+        when(questionRepository.findById(1L)).thenReturn(Optional.of(question));
+
+        // When
+        QuestionListResponse response = questionService.getOneQuestion(1L, 1L);
+
+        // Then
+        assertNotNull(response);
+        assertEquals("Sample question", response.getQuestion());
+        assertEquals(1L, response.getId());
+    }
+
+    @DisplayName("문제 개별 조회 시 문제집이 없는 경우 예외 발생")
+    @Test
+    void testGetOneQuestion_BookNotFound() {
+        when(bookRepository.findById(1L)).thenReturn(Optional.empty());
+
+        BaseException exception = assertThrows(BaseException.class, () ->
+            questionService.getOneQuestion(1L, 1L));
+
+        assertEquals(ErrorCode.BOOK_NOT_FOUND, exception.getErrorCode());
+    }
+
+    @DisplayName("문제 개별 조회 시 문제를 찾을 수 없는 경우 예외 발생")
+    @Test
+    void testGetOneQuestion_QuestionNotFound() {
+        when(bookRepository.findById(1L)).thenReturn(Optional.of(book));
+        when(questionRepository.findById(1L)).thenReturn(Optional.empty());
+
+        BaseException exception = assertThrows(BaseException.class, () ->
+            questionService.getOneQuestion(1L, 1L));
+
+        assertEquals(ErrorCode.QUESTION_NOT_FOUND, exception.getErrorCode());
     }
 
     @DisplayName("문제 수정 성공")
@@ -336,7 +414,7 @@ public class QuestionServiceTest {
         List<Question> questionList = List.of(question);
         Page<Question> questionPage = new PageImpl<>(questionList);
 
-        when(questionRepository.findByBookIdAndQuestionContaining(eq(book.getId()), anyString(), any(Pageable.class)))
+        when(questionRepository.findByBookIdAndQuestionContainingOrderByCreatedAtDesc(eq(book.getId()), anyString(), any(Pageable.class)))
             .thenReturn(questionPage);
 
         // When
