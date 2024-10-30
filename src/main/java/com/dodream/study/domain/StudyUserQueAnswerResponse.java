b18adbbd5bcf0717c7ead168fe87b38bc8c2186f
codesche <codesche@gmail.com>
2024-10-30 14:43:44 +0900

    feat: 스터디방 메인페이지 DTO 구현 (#117)

---
+++ src/main/java/com/dodream/study/domain/StudyUserQueAnswerResponse.java
@@ -0,0 +1,26 @@
+package com.dodream.study.domain;
+
+import com.dodream.book.enumtype.Evaluation;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@Builder
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class StudyUserQueAnswerResponse {
+
+    private Long questionId;
+    private String question;
+    private String modelAnswer;
+    private String username;
+    private Evaluation evaluation;
+
+    @JsonFormat(shape= JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt;
+
+}
