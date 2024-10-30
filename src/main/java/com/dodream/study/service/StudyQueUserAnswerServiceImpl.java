dc10f63801ebcc3d2d3d42f3f2cb70a3b6b43a48
codesche <codesche@gmail.com>
2024-10-30 14:43:25 +0900

    feat: 스터디방 메인페이지 서비스 구현 (#117)

---
+++ src/main/java/com/dodream/study/service/StudyQueUserAnswerServiceImpl.java
@@ -0,0 +1,60 @@
+package com.dodream.study.service;
+
+import com.dodream.book.repository.UserAnswerRepository;
+import com.dodream.common.exception.BaseException;
+import com.dodream.common.exception.ErrorCode;
+import com.dodream.study.domain.StudyUserQueAnswerResponse;
+import com.dodream.study.entity.Study;
+import com.dodream.study.repository.StudyRepository;
+import com.dodream.user.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class StudyQueUserAnswerServiceImpl implements StudyQueUserAnswerService {
+
+    private final UserAnswerRepository userAnswerRepository;
+    private final StudyRepository studyRepository;
+
+    // 전체 조회
+    @Override
+    @Transactional(readOnly = true)
+    public Page<StudyUserQueAnswerResponse> getStudyDetails(Pageable pageable, Long studyId, User user) {
+        Study study = studyRepository.findById(studyId)
+            .orElseThrow(() -> new BaseException(ErrorCode.STUDY_NOT_FOUND));
+        return userAnswerRepository.findStudyUserAnswers(pageable, study.getId(), user);
+    }
+
+    // 내가 푼 문제 조회
+    @Override
+    @Transactional(readOnly = true)
+    public Page<StudyUserQueAnswerResponse> getStudyMyDetails(Pageable pageable, Long studyId,
+        User user) {
+        Study study = studyRepository.findById(studyId)
+            .orElseThrow(() -> new BaseException(ErrorCode.STUDY_NOT_FOUND));
+        return userAnswerRepository.findStudyMyUserAnswers(pageable, study.getId(), user);
+    }
+
+    // 내가 풀지 않은 문제 조회
+    @Override
+    @Transactional(readOnly = true)
+    public Page<StudyUserQueAnswerResponse> getStudyOtherDetails(Pageable pageable, Long studyId,
+        User user) {
+        Study study = studyRepository.findById(studyId)
+            .orElseThrow(() -> new BaseException(ErrorCode.STUDY_NOT_FOUND));
+        return userAnswerRepository.findStudyOtherUserAnswers(pageable, study.getId(), user);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Page<StudyUserQueAnswerResponse> getSearchStudyUserAnswer(Pageable pageable, Long studyId,
+        User user, String keyword) {
+        Study study = studyRepository.findById(studyId)
+            .orElseThrow(() -> new BaseException(ErrorCode.STUDY_NOT_FOUND));
+        return userAnswerRepository.searchStudyUserAnswers(pageable, study.getId(), user, keyword);
+    }
+}
