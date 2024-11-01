09bf414841397875a984cd846c1b4f8bdc78f04d
seoyeon-jung <jungluchia1204@gmail.com>
2024-11-01 12:13:17 +0900

    fix: 로그인한 사용자가 좋아요 했는지 여부 확인 response에 추가 (#123)

--- src/main/java/com/dodream/book/domain/BookCommentResponse.java
+++ src/main/java/com/dodream/book/domain/BookCommentResponse.java
@@ -1,5 +1,6 @@
 package com.dodream.book.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Data;
@@ -11,7 +12,11 @@ public class BookCommentResponse {
     private String comment;
     private Long userId;                    // 작성자 ID
     private String username;                // 작성자
+    private String userProfile;     // 생성한 사람 프로필
     private Long likeCount;                 // 좋아요 수
     private Long bookId;                    // 문제집 ID
+    private boolean isLiked;   // 사용자가 북마크했는지 여부
+
+    @JsonFormat(shape= JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss")
     private LocalDateTime createdAt;        // 생성 날짜 (최신순 정렬 위해 필요)
 }
