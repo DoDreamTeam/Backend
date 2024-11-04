7a256e5f0e769ff762739c626908216173b82cf1
codesche <codesche@gmail.com>
2024-11-04 09:31:05 +0900

    feature: 알림 DTO 생성 (#128)

---
+++ src/main/java/com/dodream/notifications/domain/NotificationResponse.java
@@ -0,0 +1,33 @@
+package com.dodream.notifications.domain;
+
+import com.dodream.notifications.entity.Notifications;
+import com.dodream.notifications.enumtype.NotifyType;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class NotificationResponse {
+    private Long id;
+    private NotifyType notifyType;
+    private String toName;
+    private String content;
+
+    @JsonFormat(shape= JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime createdAt;
+    private String url;
+
+    public static NotificationResponse fromEntity(Notifications notification) {
+        return NotificationResponse.builder()
+            .id(notification.getId())
+            .notifyType(notification.getNotifyType())
+            .toName(notification.getToName())
+            .content(notification.getContent())
+            .createdAt(notification.getCreatedAt())
+            .url(notification.getUrl())
+            .build();
+    }
+
+}
