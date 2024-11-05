090596216cd1fd6d94af8a8fa30d088f97abda59
codesche <codesche@gmail.com>
2024-11-05 15:10:48 +0900

    fix: 스터디 추가 시 type 맞지 않는 이슈 해결

--- src/main/java/com/dodream/study/domain/StudyRequest.java
+++ src/main/java/com/dodream/study/domain/StudyRequest.java
@@ -3,6 +3,8 @@ package com.dodream.study.domain;
 import com.dodream.common.enumtype.Category;
 import com.dodream.study.entity.Study;
 import com.dodream.user.entity.User;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Data;
 
@@ -14,6 +16,18 @@ public class StudyRequest {
     private Category category;
     private String description;
 
+    @JsonCreator
+    public StudyRequest(
+        @JsonProperty("title") String title,
+        @JsonProperty("username") String username,
+        @JsonProperty("category") Category category,
+        @JsonProperty("description") String description
+    ) {
+        this.title = title;
+        this.username = username;
+        this.category = category;
+        this.description = description;
+    }
     public Study toEntity(User user) {
         return Study.builder()
             .title(title)
