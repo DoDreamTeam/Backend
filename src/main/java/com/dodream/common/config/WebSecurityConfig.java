dfccd00ce377e8d9ca266b98799a39e2ac05c046
seoyeon-jung <jungluchia1204@gmail.com>
2024-11-05 16:09:21 +0900

    fix: 403 오류 수정 (path 추가)

--- src/main/java/com/dodream/common/config/WebSecurityConfig.java
+++ src/main/java/com/dodream/common/config/WebSecurityConfig.java
@@ -89,7 +89,11 @@ public class WebSecurityConfig {
                     new AntPathRequestMatcher("/api/notice/*/comments", "GET"),
 
                     // 특정 스터디의 문제 조회
-                    new AntPathRequestMatcher("/api/study/*/question/*", "GET")
+                    new AntPathRequestMatcher("/api/study/*/question/*", "GET"),
+
+                    // 마이 페이지
+                    new AntPathRequestMatcher("/api/mypage/**", "GET"),
+                    new AntPathRequestMatcher("/api/mypage/book/**", "GET")
                     
                 ).permitAll()
                 // 그 밖의 다른 요청들은 인증을 통과한(로그인한) 사용자라면 모두 접근할 수 있도록 한다.
