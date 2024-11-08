4fa21fec24f6ce10f6e70adbcd14b10a5989f380
codesche <codesche@gmail.com>
2024-11-08 15:36:54 +0900

    fix: 스터디 참여 여부 관련 메서드 추가

--- src/main/java/com/dodream/study/service/StudyService.java
+++ src/main/java/com/dodream/study/service/StudyService.java
@@ -8,6 +8,7 @@ import com.dodream.study.domain.StudyUpdateResponse;
 import com.dodream.user.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 
 public interface StudyService {
     Page<StudyResponse> getStudyList(User loginedUser, Pageable pageable, String category);
@@ -18,6 +19,7 @@ public interface StudyService {
     Page<StudyResponse> getMyStudyList(Pageable pageable, User user);
     Page<StudyResponse> getPopularStudyList(Pageable pageable, User user, Long userCount);
     StudyResponse findStudy(Long id, User user);
+    boolean existStudy(Long id, User user);
 
 //    Page<StudyResponse> getMyStudy(User user, Pageable pageable, Long id);
 }
