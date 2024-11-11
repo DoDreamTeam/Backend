d67c796c6599ab329478ce80ecbdc54f17c1c18b
codesche <codesche@gmail.com>
2024-11-11 09:16:11 +0900

    fix: redis cache ttl 조정 및 일부 메소드 redis cache 기능 제거

--- src/main/java/com/dodream/common/config/RedisConfig.java
+++ src/main/java/com/dodream/common/config/RedisConfig.java
@@ -35,7 +35,7 @@ public class RedisConfig {
                 new StringRedisSerializer()))
             .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(
                 new GenericJackson2JsonRedisSerializer()))
-            .entryTtl(Duration.ofSeconds(30L));
+            .entryTtl(Duration.ofSeconds(15L));
 
         return RedisCacheManager
             .RedisCacheManagerBuilder
