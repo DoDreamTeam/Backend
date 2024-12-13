d121c67cfb57f7c4eb0a114a931993a41e61bf15
codesche <codesche@gmail.com>
2024-12-13 17:47:00 +0900

    feat: 알림 일괄 삭제 설정 기간 메소드 설정 (#144)

--- src/main/java/com/dodream/notifications/repository/NotificationRepository.java
+++ src/main/java/com/dodream/notifications/repository/NotificationRepository.java
@@ -1,6 +1,7 @@
 package com.dodream.notifications.repository;
 
 import com.dodream.notifications.entity.Notifications;
+import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -10,5 +11,6 @@ public interface NotificationRepository extends JpaRepository<Notifications, Lon
 
     List<Notifications> findByUserIdOrderByCreatedAtDesc(Long userId);
 
+    List<Notifications> findAllByCreatedAtBefore(LocalDateTime localDateTime);
 
 }
